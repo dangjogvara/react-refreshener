8637adb558da107c22a2ae6ce6eceb059633518d
Dan Poulsen <dangjogvara@gmail.com>
2022-09-30 21:25:23 +0100

    save meetup to firebase

--- src/pages/NewMeetup.tsx
+++ src/pages/NewMeetup.tsx
@@ -1,10 +1,27 @@
-import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from 'react-router-dom';
+
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 
 function NewMeetupPage() {
+    const history = useHistory();
+
+    const addMeetupHandler = (meetupData: any) => {
+        // @ts-ignore
+        fetch('https://react-refreshener-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(() => history.replace('/'));
+    };
     return <section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm />
-    </section>
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>;
 }
 
 export default NewMeetupPage;
