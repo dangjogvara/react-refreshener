cc181bcfd0f47041704ffff870e1ebac93a0e392
Dan Poulsen <dangjogvara@gmail.com>
2022-10-01 16:01:01 +0100

    change context functions

--- src/store/favorites-context.tsx
+++ src/store/favorites-context.tsx
@@ -13,6 +13,24 @@ const FavoritesContext = createContext({
 const FavoritesContextProvider = ({children}: favoritesProps) => {
     const [userFavorites, setUserFavorites] = useState([]);
 
+    const addFavoriteHandler = (favoriteMeetup: any) => {
+        setUserFavorites((prevUserFavorite) => {
+            return prevUserFavorite.concat(favoriteMeetup);
+        });
+    };
+
+    const removeFavoriteHandler = (meetupId: any) => {
+        setUserFavorites((prevUserFavorites) => {
+            // @ts-ignore
+            return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+        });
+    };
+
+    const itemIsFavorite = (meetupId: any) => {
+        // @ts-ignore
+        return userFavorites.some((meetup) => meetup.id === meetupId);
+    };
+
     const context = {
         favorites: userFavorites,
         totalFavorites: userFavorites.length
